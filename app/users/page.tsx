228b2beefb2572145e938a9f7a819c4d3eecc451
“bradeu” <bradeu01@gmail.com>
2024-01-19 23:07:24 -0800

    Modify Pages

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductCard from '../components/ProductCard'
+
+const UsersPage = () => {
+  return (
+    <>
+    <div>UsersPage</div>
+    <ProductCard/>
+    </>
+  )
+}
+
+export default UsersPage
